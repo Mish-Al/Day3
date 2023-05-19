878a3b6d2b7bf217bbede45bb3c65b8ea747a0d1
Mish-Al <mtaliep.lifechoices@gmail.com>
2023-05-19 09:16:24 +0200

    day3 complete

---
+++ script/code.js
@@ -0,0 +1,32 @@
+let string = "I am a string"
+    console.log(typeof string)
+        parseInt(string)
+// Did not change
+
+let string2 = "I am a string";
+    console.log(typeof string2);
+    
+let anArray = ["Hi","I","am","an","array"];
+    Array(anArray)
+    console.log(typeof anArray);
+
+let numb = 2;
+console.log(typeof numb);
+
+let value = 900719925124740998n;
+    console.log(typeof value);
+
+let bool = true;
+    console.log(typeof bool);
+
+let beef;
+    console.log(typeof beef);
+
+let object = {name:"yes",surname:"no"};
+    console.log(typeof object);
+
+let sym1 = Symbol();
+    console.log(typeof sym1);
+
+let yes = null;
+    console.log(typeof yes);
